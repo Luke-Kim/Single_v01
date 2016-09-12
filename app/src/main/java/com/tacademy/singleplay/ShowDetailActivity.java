ee99cb08c3fb8492c346dfbc97d8a503660b6667
Kim Joo Young <553th@naver.com>
2016-09-12 10:58:00 +0900

    push 수정

--- app/src/main/java/com/tacademy/singleplay/ShowDetailActivity.java
+++ app/src/main/java/com/tacademy/singleplay/ShowDetailActivity.java
@@ -156,7 +156,7 @@ public class ShowDetailActivity extends AppCompatActivity {
         btn_wish.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
-                if (uid != 0) {
+                if (uid == 0) {
                     Toast.makeText(ShowDetailActivity.this, "로그인 후 사용해 주세요", Toast.LENGTH_SHORT).show();
                     btn_wish.setChecked(false);
                 } else {
@@ -187,7 +187,7 @@ public class ShowDetailActivity extends AppCompatActivity {
         btn_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (signInData != null) {
+                if (uid != 0) {
                     BookingListAddRequest request = new BookingListAddRequest(MyApplication.getContext());
                     NetworkManager.getInstance().getNetworkData(request, new NetworkManager.OnResultListener<BookingListAdd>() {
                                 @Override
@@ -205,16 +205,15 @@ public class ShowDetailActivity extends AppCompatActivity {
                     startActivity(intent);
                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
                     intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                } else if(uid != 0) {
+                } else
                     Toast.makeText(ShowDetailActivity.this, "회원가입 후 사용해주세요", Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(ShowDetailActivity.this, LoginActivity.class);
                     startActivity(intent);
                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
                     intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 }
-            }
-        });
-    }
+            });
+        }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
